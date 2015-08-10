6d231d1b201de2834f0c492b0beb0a3a7fdd741d
jxnblk <jacksonblack@gmail.com>
2015-08-10 12:10:23 -0400

    Use CSS animations for logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,47 +3,52 @@ import { assign, clone } from 'lodash'
 import React from 'react'
 import { colors } from '../util/styles'
 
+const offset1 = 15
+const offset2 = 45
+const offset3 = 60
+const rotationKeyframes = (
+`
+/* <![CDATA[ */
+@-webkit-keyframes rotate1 {
+    0% { -webkit-transform: rotate3d(0, 1, 1, ${  0 + offset1}deg) }
+   50% { -webkit-transform: rotate3d(0, 1, 1, ${180 + offset1}deg) }
+  100% { -webkit-transform: rotate3d(0, 1, 1, ${360 + offset1}deg) }
+}
+@keyframes rotate1 {
+    0% { transform: rotate3d(0, 1, 1, ${  0 + offset1}deg) }
+   50% { transform: rotate3d(0, 1, 1, ${180 + offset1}deg) }
+  100% { transform: rotate3d(0, 1, 1, ${360 + offset1}deg) }
+}
+
+@-webkit-keyframes rotate2 {
+    0% { -webkit-transform: rotate3d(1, 0, 1, ${  0 + offset2}deg) }
+   50% { -webkit-transform: rotate3d(1, 0, 1, ${180 + offset2}deg) }
+  100% { -webkit-transform: rotate3d(1, 0, 1, ${360 + offset2}deg) }
+}
+@keyframes rotate2 {
+    0% { transform: rotate3d(1, 0, 1, ${  0 + offset2}deg) }
+   50% { transform: rotate3d(1, 0, 1, ${180 + offset2}deg) }
+  100% { transform: rotate3d(1, 0, 1, ${360 + offset2}deg) }
+}
+
+@-webkit-keyframes rotate3 {
+    0% { -webkit-transform: rotate3d(1, 1, 0, ${  0 + offset3}deg) }
+   50% { -webkit-transform: rotate3d(1, 1, 0, ${180 + offset3}deg) }
+  100% { -webkit-transform: rotate3d(1, 1, 0, ${360 + offset3}deg) }
+}
+@keyframes rotate3 {
+    0% { transform: rotate3d(1, 1, 0, ${  0 + offset3}deg) }
+   50% { transform: rotate3d(1, 1, 0, ${180 + offset3}deg) }
+  100% { transform: rotate3d(1, 1, 0, ${360 + offset3}deg) }
+}
+/* ]]> */
+`
+).replace(/\n/g, ' ').replace(/\s\s+/g, ' ')
+
 class Logo extends React.Component {
-
-  constructor() {
-    super()
-    this.state = {
-      rot: 0,
-      timer: false
-    }
-    this.handleClick = this.handleClick.bind(this)
-    this.incrementRotation = this.incrementRotation.bind(this)
-  }
-
-  incrementRotation() {
-    let { rot } = this.state
-    rot += 720
-    this.setState({ rot })
-    let timer = window.setTimeout(function () {
-      this.incrementRotation()
-    }.bind(this), 5000)
-    this.setState({ timer })
-  }
-
-  componentDidMount() {
-    this.incrementRotation()
-  }
-
-  componentWillUnmount() {
-    let { timer } = this.state
-    window.clearTimeout(timer)
-  }
-
-  handleClick(e) {
-    console.log('click')
-    let { rot } = this.state
-    rot += 90
-    this.setState({ rot })
-  }
 
   render() {
     const { style } = this.props
-    const { rot } = this.state
 
     const size = 128
     let c = size / 2
@@ -51,39 +56,33 @@ class Logo extends React.Component {
     let r1 = size / 2 - s1
     let r2 = size * 2/5
 
-    let s = {
+    const ringStyles = {
+      fill: 'none',
+      stroke: 'currentcolor',
+      display: 'block',
+      strokeWidth: s1,
+      vectorEffect: 'non-scaling-stroke',
+      opacity: .5
+    }
+
+    const s = {
       svg: assign(style, {
         display: 'inline-block',
         fill: 'currentcolor'
       }),
-      c1: {
-        fill: 'none',
-        stroke: 'currentcolor',
-        display: 'block',
-        strokeWidth: s1,
-        vectorEffect: 'non-scaling-stroke',
-        opacity: .5,
-        WebkitTransformOrigin: '50% 50%',
-        transformOrigin: '50% 50%',
-        WebkitTransform: `rotate3d(0, 1, 1, ${rot}deg)`,
-        transform:       `rotate3d(0, 1, 1, ${rot}deg)`,
-        WebkitTransition: 'transform 5s linear',
-        transition: 'transform 5s linear'
-      },
-      // c4: {
-      //   opacity: .0
-      // }
+      c1: assign({}, ringStyles, {
+        WebkitAnimation: 'rotate1 2s linear 0s infinite',
+        animation: 'rotate1 2s linear 0s infinite',
+      }),
+      c2: assign({}, ringStyles, {
+        WebkitAnimation: 'rotate2 2s linear 0s infinite',
+        animation: 'rotate2 2s linear 0s infinite',
+      }),
+      c3: assign({}, ringStyles, {
+        WebkitAnimation: 'rotate3 2s linear 0s infinite',
+        animation: 'rotate3 2s linear 0s infinite',
+      })
     }
-
-    s.c2 = assign(clone(s.c1), {
-      WebkitTransform: `rotate3d(1, 0, 0, ${-45+rot}deg)`,
-      transform:       `rotate3d(1, 0, 0, ${-45+rot}deg)`,
-    })
-
-    s.c3 = assign(clone(s.c1), {
-      WebkitTransform: `rotate3d(1, 0, 1, ${90+rot}deg)`,
-      transform:       `rotate3d(1, 0, 1, ${90+rot}deg)`,
-    })
 
     return (
       <svg
@@ -91,10 +90,14 @@ class Logo extends React.Component {
         height={size}
         viewBox={[0,0, size, size].join(' ')}
         onClick={this.handleClick}
+        className='Logo'
         style={s.svg}>
-        <circle cx={c} cy={c} r={r1} style={s.c1} />
-        <circle cx={c} cy={c} r={r1} style={s.c2} />
-        <circle cx={c} cy={c} r={r1} style={s.c3} />
+        <style dangerouslySetInnerHTML={{ __html: rotationKeyframes }} />
+        <g transform={`translate(${c}, ${c})`}>
+          <circle cx={0} cy={0} r={r1} style={s.c1} />
+          <circle cx={0} cy={0} r={r1} style={s.c2} />
+          <circle cx={0} cy={0} r={r1} style={s.c3} />
+        </g>
       </svg>
     )
   }
